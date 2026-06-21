859bc97780502324b1a6ad6bbbcc88034a427a81
Dhakshin Kumar <dhakshinsai38@gmail.com>
2026-06-21 13:07:14 +0530

    feat(C9): implement V0-compatible JSON serializer

--- windows-agent/src/event_serializer.cpp
+++ windows-agent/src/event_serializer.cpp
@@ -1,6 +1,43 @@
 #include "event_serializer.h"
 
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
 std::string serialize_event(const GhostEvent& event)
 {
-    return "{}";
+    json j;
+
+    j["ts"]   = event.ts;
+    j["pid"]  = event.pid;
+    j["ppid"] = event.ppid;
+
+    j["uid"]  = event.uid;
+    j["gid"]  = event.gid;
+
+    j["comm"] = event.comm;
+    j["type"] = event.type;
+
+    if (event.file.has_value())
+        j["file"] = event.file.value();
+
+    if (event.args.has_value())
+        j["args"] = event.args.value();
+
+    if (event.flags.has_value())
+        j["flags"] = event.flags.value();
+
+    if (event.daddr.has_value())
+        j["daddr"] = event.daddr.value();
+
+    if (event.dport.has_value())
+        j["dport"] = event.dport.value();
+
+    if (event.family.has_value())
+        j["family"] = event.family.value();
+
+    if (event.clone_flags.has_value())
+        j["clone_flags"] = event.clone_flags.value();
+
+    return j.dump();
 }
